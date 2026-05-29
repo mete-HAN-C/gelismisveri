a2af259ce391f9dd499ea7b7b2072bb728dc9b8e
mete-HAN-C <mete123sener@gmail.com>
2026-05-29 23:15:48 +0300

    Delete GVT_PROJE_SON directory

--- GVT_PROJE_SON/ml_pipeline/config.py
+++
@@ -1,9 +0,0 @@
-SERVER   = r"localhost"
-DATABASE = "FintechDB"
-
-CONN_STR = (
-    f"Driver={{ODBC Driver 17 for SQL Server}};"
-    f"Server={SERVER};"
-    f"Database={DATABASE};"
-    f"Trusted_Connection=yes;"
-)
